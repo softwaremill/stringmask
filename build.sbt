c3eb3048e2e2fffc175fc4030bbfd3b2b44b824e
Krzysztof Ciesielski <krzysztof.ciesielski@softwaremill.com>
2016-03-23 18:35:12 +0100

    Add Scala 2.10 compatibility

--- build.sbt
+++ build.sbt
@@ -3,10 +3,14 @@ name := "stringmask"
 
 version := "1.0.0-SNAPSHOT"
 
+crossScalaVersions := Seq("2.10.6", "2.11.7")
+
 scalaVersion := "2.11.7"
 
 libraryDependencies ++= Seq(
-  "org.scala-lang" % "scala-reflect" % "2.11.7",
+  "org.scala-lang" % "scala-reflect" % scalaVersion.value,
+  "org.scala-lang" % "scala-compiler" % scalaVersion.value,
+  "org.typelevel" %% "macro-compat" % "1.1.1",
   "org.scalatest" %% "scalatest" % "2.2.6" % "test"
 )
 
--- src/main/scala/com/softwaremill/macros/customize/CustomizeImpl.scala
+++ src/main/scala/com/softwaremill/macros/customize/CustomizeImpl.scala
@@ -3,6 +3,7 @@ package com.softwaremill.macros.customize
 import scala.annotation.StaticAnnotation
 import scala.language.experimental.macros
 import scala.reflect.macros.whitebox
+import macrocompat.bundle
 
 class customize extends StaticAnnotation {
   def macroTransform(annottees: Any*): Any = macro CustomizeImpl.impl
@@ -10,31 +11,34 @@ class customize extends StaticAnnotation {
 
 class mask extends StaticAnnotation
 
+@bundle
+class CustomizeImpl(val c: whitebox.Context) {
 
-object CustomizeImpl {
-
-  def impl(c: whitebox.Context)(annottees: c.Expr[Any]*): c.Expr[Any] = {
+  def impl(annottees: c.Expr[Any]*): c.Expr[Any] = {
     import c.universe._
 
     def extractCaseClassesParts(classDecl: ClassDef) = classDecl match {
-      case q"case class $className(..$fields) extends ..$parents { ..$body }" =>
+      case q"""case class $className(..$fields) extends ..$parents { ..$body }""" =>
         (className, fields, parents, body)
     }
 
     def extractNewToString(typeName: TypeName, allFields: List[Tree]) = {
       val fieldListTree = allFields.foldLeft(List.empty[Tree]) { case (accList, fieldTree) =>
         fieldTree match {
-          case q"${Modifiers(fs, nm, List(q"new mask()"))} val $field: $fieldType = $sth" =>
-            accList :+ Literal(Constant("***"))
           case q"$m val $field: $fieldType = $sth" =>
-            accList :+ q"$field.toString"
+            m.annotations match {
+              case List(q"new mask()") =>
+                accList :+ Literal(Constant("***"))
+              case _ =>
+                accList :+ q"$field.toString"
+            }
           case _ => c.abort(c.enclosingPosition, "Cannot call .toString on tree " + showRaw(fieldTree))
         }
       }
       val treesAsTuple = Apply(Select(Ident(TermName("scala")), TermName("Tuple" + fieldListTree.length)), fieldListTree)
       val typeNameStrTree = Literal(Constant(typeName.toString))
       q"""
-         override def toString: ${typeOf[String]} = {
+         override def toString = {
           $typeNameStrTree + $treesAsTuple
          }
       """
