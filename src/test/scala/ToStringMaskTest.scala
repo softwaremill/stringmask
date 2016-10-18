6029cc8b582b89e633c11f6eef5e3561d285d823
Marcin Kubala <marcin@mkubala.pl>
2016-10-18 14:47:43 +0200

    Added support for classes that have more than 22 fields.
    Current limitation came from the scala.Tuple-based implementation of the `toString` generated by Stringmask macro.
    This commit introduces a new reduceLeftOption-based solution which allows us to get rid of `scala.Tuple`s from the generated code.

--- src/test/scala/ToStringMaskTest.scala
+++ src/test/scala/ToStringMaskTest.scala
@@ -42,6 +42,30 @@ class ToStringMaskTest extends FlatSpec with Matchers {
     u.toString should be("User40(3,***,null)")
   }
 
+  it should "work on classes without any fields" in {
+    // given
+    val ec = EmptyClass()
+
+    // then
+    ec.toString should be("EmptyClass()")
+  }
+
+  it should "work on classes with 1 field" in {
+    // given
+    val sc = SmallClass(100)
+
+    // then
+    sc.toString should be("SmallClass(***)")
+  }
+
+  it should "work on classes with > 22 fields" in {
+    // given
+    val bc = BigClass(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 ,14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25)
+
+    // then
+    bc.toString should be("BigClass(***,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25)")
+  }
+
 }
 
 @customize
@@ -52,6 +76,17 @@ case class User40(id: Int, @mask email: String, something: String)
 
 @customize
 case class UserPrivate private(id: Int, @mask email: String)
+
+@customize
+case class EmptyClass()
+
+@customize
+case class SmallClass(@mask n: Int)
+
+@customize
+case class BigClass(@mask a1: Int, a2: Int, a3: Int, a4: Int, a5: Int, a6: Int, a7: Int, a8: Int, a9: Int, a10: Int,
+                    a11: Int, a12: Int, a13: Int, a14: Int,a15: Int, a16: Int, a17: Int, a18: Int, a19: Int, a20: Int,
+                    a21: Int, a22: Int, a23: Int, a24: Int, a25: Int)
 
 object UserPrivate {
   def apply(email: String) = new UserPrivate(1, email)
